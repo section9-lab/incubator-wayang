a718bfd90b38c034d8c2810f87247b4dca5bf677
Mohit Bisht <bishtmohit2405@gmail.com>
2026-03-04 14:23:06 +0530

    Fix GenericJdbcTableSource jdbcName handling causing incorrect configuration lookup

--- wayang-platforms/wayang-generic-jdbc/src/test/java/org/apache/wayang/genericjdbc/operators/GenericJdbcJoinOperatorTest.java
+++ wayang-platforms/wayang-generic-jdbc/src/test/java/org/apache/wayang/genericjdbc/operators/GenericJdbcJoinOperatorTest.java
@@ -1,5 +1,4 @@
 package org.apache.wayang.genericjdbc.operators;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,35 +15,96 @@ package org.apache.wayang.genericjdbc.operators;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import org.apache.wayang.basic.data.Record;
+import org.apache.wayang.basic.data.Tuple2;
+import org.apache.wayang.basic.operators.JoinOperator;
+import org.apache.wayang.basic.operators.LocalCallbackSink;
+import org.apache.wayang.core.api.Configuration;
+import org.apache.wayang.core.api.WayangContext;
 import org.apache.wayang.core.function.TransformationDescriptor;
-import org.junit.Test;
+import org.apache.wayang.core.plan.wayangplan.WayangPlan;
+import org.apache.wayang.genericjdbc.GenericJdbc;
+import org.apache.wayang.genericjdbc.platform.GenericJdbcPlatform;
+import org.apache.wayang.java.platform.JavaPlatform;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-public class GenericJdbcJoinOperatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GenericJdbcJoinOperatorTest {
 
     @Test
-    public void testOperatorCreation() {
+    void testJoinExecution() throws Exception {
+
+        String url = "jdbc:hsqldb:mem:wayang_test_db;DB_CLOSE_DELAY=-1";
+
+        Class.forName("org.hsqldb.jdbcDriver");
+
+        try (Connection conn = DriverManager.getConnection(url, "SA", "")) {
+            Statement stmt = conn.createStatement();
+
+            stmt.execute("CREATE TABLE T1 (A INT, VAL1 VARCHAR(20));");
+            stmt.execute("INSERT INTO T1 VALUES (1, 'Apache');");
+
+            stmt.execute("CREATE TABLE T2 (A INT, VAL2 INT);");
+            stmt.execute("INSERT INTO T2 VALUES (1, 2026);");
+        }
+
+        Configuration config = new Configuration();
+        config.setProperty("wayang.genericjdbc.jdbc.url", url);
+        config.setProperty("wayang.genericjdbc.jdbc.user", "SA");
+        config.setProperty("wayang.genericjdbc.jdbc.password", "");
+        config.setProperty("wayang.genericjdbc.jdbc.driverName", "org.hsqldb.jdbcDriver");
+
+        GenericJdbcTableSource source1 = new GenericJdbcTableSource("T1", "A", "VAL1");
+        GenericJdbcTableSource source2 = new GenericJdbcTableSource("T2", "A", "VAL2");
+
+        TransformationDescriptor<Record, Integer> keyExtractor0 =
+                new TransformationDescriptor<>(
+                        r -> (Integer) r.getField(0),
+                        Record.class,
+                        Integer.class
+                ).withSqlImplementation("T1", "A");
 
         TransformationDescriptor<Record, Integer> keyExtractor1 =
                 new TransformationDescriptor<>(
-                        (Record record) -> (Integer) record.getField(0),
+                        r -> (Integer) r.getField(0),
                         Record.class,
                         Integer.class
+                ).withSqlImplementation("T2", "A");
+
+        JoinOperator<Record, Record, Integer> join =
+                new JoinOperator<>(keyExtractor0, keyExtractor1);
+
+        List<Tuple2<Record, Record>> results = new ArrayList<>();
+
+        @SuppressWarnings("unchecked")
+        LocalCallbackSink<Tuple2<Record, Record>> sink =
+                LocalCallbackSink.createCollectingSink(
+                        results,
+                        (Class<Tuple2<Record, Record>>) (Class<?>) Tuple2.class
                 );
 
-        TransformationDescriptor<Record, Integer> keyExtractor2 =
-                new TransformationDescriptor<>(
-                        (Record record) -> (Integer) record.getField(0),
-                        Record.class,
-                        Integer.class
-                );
+        source1.addTargetPlatform(GenericJdbcPlatform.getInstance());
+        source2.addTargetPlatform(GenericJdbcPlatform.getInstance());
+        join.addTargetPlatform(JavaPlatform.getInstance());
+        sink.addTargetPlatform(JavaPlatform.getInstance());
 
-        GenericJdbcJoinOperator<Integer> operator =
-                new GenericJdbcJoinOperator<>(keyExtractor1, keyExtractor2);
+        source1.connectTo(0, join, 0);
+        source2.connectTo(0, join, 1);
+        join.connectTo(0, sink, 0);
 
-        assertNotNull(operator);
+        WayangContext ctx = new WayangContext(config)
+                .with(GenericJdbc.plugin())
+                .with(org.apache.wayang.java.Java.basicPlugin());
+
+        ctx.execute(new WayangPlan(sink));
+
+        assertEquals(1, results.size());
     }
 }
